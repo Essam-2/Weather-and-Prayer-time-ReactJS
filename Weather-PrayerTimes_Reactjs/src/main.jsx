8c2b2aa5439121401742e4da6e1831ff33bad4ee
Essam <esamanwar2@gmail.com>
2024-02-24 09:53:00 +0300

    [+] css file react-country-state-city

--- Weather-PrayerTimes_Reactjs/src/main.jsx
+++ Weather-PrayerTimes_Reactjs/src/main.jsx
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "tw-elements-react/dist/css/tw-elements-react.min.css";
+import "react-country-state-city/dist/react-country-state-city.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
